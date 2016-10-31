f520af3122ec665891710aa2ba0c8dd8d892c5de
Ethan Winters <ewinters@us.ibm.com>
2016-10-31 15:56:38 -0500

    lint fixes

--- src/layouts/show-locations/index.js
+++ src/layouts/show-locations/index.js
@@ -272,18 +272,18 @@ ShowLocations.prototype.addDetails = function() {
 };
 
 ShowLocations.prototype.convertColor = function(color) {
-	function rgb2hex(rgb){
+	function rgb2hex(rgb) {
 		rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
 		return (rgb && rgb.length === 4) ?
 		("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
 		("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
 		("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
 	}
-	if(color.indexOf('#') > -1)
+	if (color.indexOf('#') > -1)
 		return color.replace('#', '');
 	else
 		return rgb2hex(color);
-}
+};
 
 ShowLocations.prototype.drawLocations = function() {
 	var current = getState();
