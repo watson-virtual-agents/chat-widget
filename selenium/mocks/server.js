3eec282a5792d4c90024993c7f7326ae70a93844
Ethan Winters <ewinters@us.ibm.com>
2016-11-08 13:27:16 -0600

    to spaces, more will need to be done when we move to travis

--- selenium/mocks/server.js
+++ selenium/mocks/server.js
@@ -4,40 +4,40 @@ var PORT = 3201;
 var botID = '77', chatID = '42', message;
 
 function router(handle, pathname, req, res) {
-	if (typeof handle[pathname] === 'function') {
-		handle[pathname](req, res);
-	} else {
-		res.writeHead(404);
-		res.end();
-	}
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](req, res);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 }
 
 function onRequest(req, res) {
-	var pathname = url.parse(req.url).pathname;
-	router(handle, pathname, req, res);
+  var pathname = url.parse(req.url).pathname;
+  router(handle, pathname, req, res);
 }
 
 function init(req, res) {
-	var response = JSON.stringify({
-		"bot_id": "77",
-		"dialog_id": "42",
-		"message": {
-			"text": ["Hi my name is Virtual Agent. I am here to answer questions about our company. What can I help you with?"]
-		}
-	});
-	res.writeHead(200, { 'Content-Type': 'application/json' });
-	res.end(response);
+  var response = JSON.stringify({
+    "bot_id": "77",
+    "dialog_id": "42",
+    "message": {
+      "text": ["Hi my name is Virtual Agent. I am here to answer questions about our company. What can I help you with?"]
+    }
+  });
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(response);
 }
 
 function receive(req, res) {
-	var string = JSON.stringify(message);
-	res.writeHead(200, { 'Content-Type': 'application/json' });
-	res.end(string);
+  var string = JSON.stringify(message);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(string);
 }
 
 function setMessage(req, res) {
-	res.writeHead(200);
-	res.end();
+  res.writeHead(200);
+  res.end();
 }
 
 var handle = {};
@@ -46,5 +46,5 @@ handle['/bots/'+ botID +'/dialogs/'+ chatID +'/messages'] = receive;
 handle['/setmessage'] = setMessage;
 
 http.createServer(onRequest).listen(PORT, function() {
-	console.log("Server has started and listening on "+ PORT );
+  console.log("Server has started and listening on "+ PORT );
 });
