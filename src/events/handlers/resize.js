7541167519e2daa99237392aec5372ea904f2d7c
Ethan Winters <ewinters@us.ibm.com>
2016-11-28 15:01:12 -0600

    if using playback

--- src/events/handlers/resize.js
+++ src/events/handlers/resize.js
@@ -18,7 +18,8 @@ function resize() {
   setTimeout(function() {
     var current = state.getState();
     if (current.active) {
-      current.chatHolder.style.maxHeight = (current.root.getBoundingClientRect().height - current.inputHolder.getBoundingClientRect().height) + 'px';
+      var inputHeight = (current.inputHolder) ? (current.inputHolder.getBoundingClientRect().height) : 0;
+      current.chatHolder.style.maxHeight = (current.root.getBoundingClientRect().height - inputHeight) + 'px';
       current.chatHolder.style.maxWidth = ((current.root.getBoundingClientRect().width > 288) ? current.root.getBoundingClientRect().width : 288) + 'px';
       if (current.root.getBoundingClientRect().width >= 480)
         current.root.classList.add('IBMChat-isLarge');
