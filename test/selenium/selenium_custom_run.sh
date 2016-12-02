46fb358923c6b71a4a5a32da329020e9a37ace90
najimovi <najimovi@ar.ibm.com>
2016-12-02 17:48:36 -0300

    stop running download.js for ff

--- test/selenium/selenium_custom_run.sh
+++ test/selenium/selenium_custom_run.sh
@@ -5,7 +5,7 @@ set -ev
 # test on all platforms once per day (cron job
 # activated in Travis UI)
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
-    npm run test:selenium:all;
+    npm run test:selenium:travis-all-platforms;
 else
     npm run test:selenium:travis-chrome;
 fi
