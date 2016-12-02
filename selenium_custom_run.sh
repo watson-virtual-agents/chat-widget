69ddaaa53f32ed9d997fd8b1d83ee6bec08c54ae
najimovi <najimovi@ar.ibm.com>
2016-12-02 00:43:16 -0300

    typo

--- selenium_custom_run.sh
+++ selenium_custom_run.sh
@@ -4,7 +4,8 @@ set -ev
 
 # test on all platforms once per day (cron job
 # activated in Travis UI)
-if [ ${TRAVIS_EVENT_TYPE} = "cron" ]; then
+# if [ ${TRAVIS_EVENT_TYPE} = "cron" ]; then
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
     npm run test:selenium:all;
 else
     npm run test:selenium:travis-chrome;
