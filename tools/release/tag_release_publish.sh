3575015c0136ff50fccabf722a95f7a9a44a3fba
najimovi <najimovi@ar.ibm.com>
2017-01-09 12:00:03 -0300

    build(changelog): added script to tag, release and publish

---
+++ tools/release/tag_release_publish.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Releases new version automating these steps:
+# a) tagging the release
+# b) creating new GitHub release
+# c) publishing package to npm
+
+# NOTES
+# - based on https://gist.github.com/stevemao/280ef22ee861323993a0
+# - should be run on 'master' branch
+version=`cat package.json | json version` &&
+git tag -a $version -m "Release $version" &&
+git push --follow-tags &&
+conventional-github-releaser -p "angular" &&
+npm publish
--- tools/release/update_changelog.sh
+++ tools/release/update_changelog.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Updates CHANGELOG.md and commits the file to repo.
+# Notes:
+# - should be run on 'dev' branch, once all changes for the release
+# have been committed
+# - assumes package.json already has the right version.
 conventional-changelog -i CHANGELOG.md -s -p "angular" &&
 git add CHANGELOG.md &&
 version=`cat package.json | json version` &&
